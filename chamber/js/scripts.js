f204c010a4dc2db3081519cc87fa0ba70b9fbf67
MiguelSecadesBYU <sec21002@byui.edu>
2022-01-26 18:02:47 +0100

    first commit

---
+++ chamber/js/scripts.js
@@ -0,0 +1,16 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x= document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
+
+// select the elements to manipulate (output to)
+const datefieldUK = document.querySelector("date"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full" }).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
