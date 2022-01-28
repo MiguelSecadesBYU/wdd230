57ae7cc9a847c011d60233e56830650ff341b5be
MiguelSecadesBYU <sec21002@byui.edu>
2022-01-28 15:51:08 +0100

    finalizing content

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,3 +1,9 @@
+const year = document.querySelector('#year');
+year.innerHTML = new Date().getFullYear();
+
+
+document.getElementById("modify").innerHTML = document.lastModified
+
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
