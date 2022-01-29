6086124dad45a741bea13b1ed73a62586abab6b1
MiguelSecadesBYU <sec21002@byui.edu>
2022-01-29 18:04:36 +0100

    deleting comments

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -16,8 +16,6 @@ x.onclick = toggleMenu;
 const datefieldUK = document.querySelector("date"); // for european/family history format with day first.
 
 // derive the current date using a date object
-//const now = new Date();
-//const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full" }).format(now);
 
 const today = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", {
@@ -25,4 +23,4 @@ const fulldate = new Intl.DateTimeFormat("en-UK", {
 
 document.getElementById('currentDate').innerHTML = `<em>${fulldate}</em>`;
 
-//datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
