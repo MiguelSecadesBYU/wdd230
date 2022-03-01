867adac2e6f7bcf45c3d7669ad63831e866be912
MiguelSecadesBYU <sec21002@byui.edu>
2022-03-01 18:09:32 +0100

    business directory first commit

---
+++ chamber/js/directory.js
@@ -0,0 +1,44 @@
+const requestURL = 'http://127.0.0.1:5500/chamber/data/data.json';
+const cards = document.querySelector('.directory');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const business = jsonObject['business'];
+    business.forEach(displayBusiness);
+  });
+
+
+  function displayBusiness(business) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
+    
+  
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. 
+    logo.setAttribute('src', business.imageurl);
+    logo.setAttribute('alt', 'Logo of  ' + business.name);
+    logo.setAttribute('loading', 'lazy');
+     
+   
+    // Change the textContent property of the p elements to contain the business info
+    address.textContent = business.address;
+    phone.textContent = business.phone;
+    website.textContent = business.website;
+  
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    cards.appendChild(card);
+  }
