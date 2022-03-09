fc0870a7c77bd4625f6451eefa50a6d9033420e0
MiguelSecadesBYU <sec21002@byui.edu>
2022-03-09 09:23:43 +0100

    fixing windchill.js

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,8 +1,11 @@
 const temp = document.getElementById("temperature");
 const wspeed = document.getElementById("windSped");
 
-const windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wspeed,0.16)) + (0.4275*temp*Math.pow(wspeed,0.16));
+if (temp>=50 && wspeed>3){
+    const windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wspeed,0.16)) + (0.4275*temp*Math.pow(wspeed,0.16));
+    const roundedWindChill= Math.round(windChill);
+    document.getElementById("windChill").innerHTML= roundedWindChill;
+}else {
+    document.getElementById("windChill").innerHTML = "N/A";
 
-const roundedWindChill= Math.round(windChill);
-document.getElementById("windChill").innerHTML= roundedWindChill;
-//document.getElementById('windChill').innerHTML = `<em>${roundedWindChill}</em>`;
+}
