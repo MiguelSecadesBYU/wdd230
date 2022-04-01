191a4d06440146c4e2d1952746310dd4e46f7041
MiguelSecadesBYU <sec21002@byui.edu>
2022-04-01 11:14:17 +0200

    coninuing with temples.html

--- temple_inn/js/temples.js
+++ temple_inn/js/temples.js
@@ -16,36 +16,41 @@ fetch(requestURL)
     // Create elements to add to the document
     let card = document.createElement('section');
     let image = document.createElement('img');
-    let temple_name = document.createElement('em');
-    let location = docuent.createElement('p');
-    let address = document.createElement('p');
+    let temple_name = document.createElement('h2');
+    let location = document.createElement('p');
+    let address  = document.createElement('p');
     let phone = document.createElement('p');
     let email = document.createElement('p');
     let services = document.createElement('p');
+    let like = document.createElement('img');
     
   
     
-    logo.setAttribute('src', temple.imageurl);
-    logo.setAttribute('alt', 'Logo of  ' + tample.templeName);
-    logo.setAttribute('loading', 'lazy');
+    image.setAttribute('src', temple.imageUrl);
+    image.setAttribute('alt', 'images/like.png');
+    image.setAttribute('loading', 'lazy');
+
+    like.setAttribute('src', temple.imageUrl);
      
    
     
     temple_name.textContent = temple.templeName;
-    address.textContent = temple.address;
-    phone.textContent = temple.phone;
-    email.textContent = temple.email;
-    services.textContent = temple.services;
+    address .textContent = 'Address: ' + temple.address;
+    location.textContent = 'Location: ' + temple.location;
+    phone.textContent = 'Phone: ' + temple.phone;
+    email.textContent = 'Email: ' + temple.email;
+    services.textContent = 'Services: ' + temple.services;
   
 
     // Add/append the section(card) with the h2 element
-    card.appendChild(temple_name);
-    card.appendChild(logo);
+    card.appendChild(image);
+    card.appendChild(temple_name); 
     card.appendChild(address);
+    card.appendChild(location);
     card.appendChild(phone);
     card.appendChild(email);
     card.appendChild(services);
-
+    card.appendChild(like);
       
     // Add/append the existing HTML div with the cards class with the section(card)
     cards.appendChild(card);
